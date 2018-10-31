321aa7cb6feb11b50735532f654d97a4588f96f6
Georg Dimopoulos <georgdimopoulos@gmail.com>
2018-10-31 13:11:08 +0200

    First General Uploads

---
+++ TYPETAppointmentWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TYPETAppointmentWebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Πλατφόρμα για online κλείσιμο με ιατρούς στο Τ.Υ.Π.Ε.Τ.";
+        }
+    }
+}
